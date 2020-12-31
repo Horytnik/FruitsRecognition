f093a2e103b585205934e76b5be40e4255b2d7a6
Horytnik <Qh33yphsGit>
2020-12-31 16:05:11 +0100

    Small changes in model layers, around 95% accuracy

--- fruitDetectionModel.py
+++ fruitDetectionModel.py
@@ -3,7 +3,6 @@ import os
 from os.path import isfile
 import numpy as np
 from sklearn.utils import shuffle
-from sklearn.model_selection import train_test_split
 from keras import Sequential
 from keras.layers import Conv2D, Dense, Dropout, MaxPooling2D, Flatten, Activation, BatchNormalization
 from keras.utils import to_categorical
@@ -13,6 +12,9 @@ import matplotlib.pyplot as plt
 import pickle
 
 startTime = time.time()
+
+trainPartSplit = 0.2
+testPartSplit = 0.05
 
 photosMainPath = "D:/Studia_PJATK/FruitsRecognition/FruitsRecogintion/FruitsImages/"
 photosPaths = []
@@ -32,29 +34,40 @@ for folder in os.listdir(photosMainPath):
 dfImagesPaths = pd.DataFrame(photosPaths, columns=["Fruit", "Path"])
 
 # Amount of each fruit photos
+plt.figure()
 plt.bar(dfImagesPaths.Fruit.value_counts().index,dfImagesPaths.Fruit.value_counts())
 plt.xticks(rotation='vertical')
+plt.title("Amount of images of each fruit")
 plt.show()
-
-# Mapping fruit names to numbers
-fruitNum = np.linspace(0, len(dfImagesPaths.Fruit.unique())-1,  len(dfImagesPaths.Fruit.unique()), dtype=int)
-mappedNames = dict(zip(dfImagesPaths["Fruit"].unique(), fruitNum.T))
-dfImagesPaths["Fruit"] = dfImagesPaths["Fruit"].map(mappedNames)
-dfImagesPaths.Fruit = dfImagesPaths.Fruit.astype(int)
 
 # Shuffle for splitting them
 dfImagesPaths = shuffle(dfImagesPaths)
 dfImagesPaths = dfImagesPaths.reset_index(drop=True)
 
+plt.figure()
+plt.bar(dfImagesPaths.Fruit[0:int(trainPartSplit * len(dfImagesPaths.Fruit))].value_counts().index,dfImagesPaths.Fruit[0:int(trainPartSplit * len(dfImagesPaths.Fruit))].value_counts())
+plt.xticks(rotation='vertical')
+plt.title("Randomly selected {}% of whole dataset to train".format(trainPartSplit*100))
+plt.show()
+
+# Mapping fruit names to numbers
+fruitNum = np.linspace(0, len(dfImagesPaths.Fruit.unique())-1,  len(dfImagesPaths.Fruit.unique()), dtype=int)
+mappedNames = dict(zip(sorted(dfImagesPaths["Fruit"].unique()), fruitNum.T))
+dfImagesPaths["Fruit"] = dfImagesPaths["Fruit"].map(mappedNames)
+dfImagesPaths.Fruit = dfImagesPaths.Fruit.astype(int)
+
+
 # imgTrain, imgTest , labelTrain, labelTest = train_test_split(dfImagesPaths.Path, dfImagesPaths.Fruit, test_size=0.1)
 
-imgTrain = dfImagesPaths.Path[0:int(0.2 * len(dfImagesPaths.Path))]
-imgTest = dfImagesPaths.Path[int(-0.05 * len(dfImagesPaths.Path)):]
 
-labelTrain = dfImagesPaths.Fruit[0:int(0.2 * len(dfImagesPaths.Fruit))]
-labelTest = dfImagesPaths.Fruit[int(-0.05 * len(dfImagesPaths.Fruit)):]
+imgTrain = dfImagesPaths.Path[0:int(trainPartSplit * len(dfImagesPaths.Path))]
+imgTest = dfImagesPaths.Path[int(-testPartSplit * len(dfImagesPaths.Path)):]
+
+labelTrain = dfImagesPaths.Fruit[0:int(trainPartSplit * len(dfImagesPaths.Fruit))]
+labelTest = dfImagesPaths.Fruit[int(-testPartSplit * len(dfImagesPaths.Fruit)):]
 
 labelTrain = to_categorical(labelTrain)
+
 
 
 # CNN model creation
@@ -66,11 +79,15 @@ model = Sequential()
 # CNN layer with 64 filters, each one have 3x3 size. Pandding same means that we don't change size of the image
 model.add(Conv2D(filters=32, kernel_size=(3,3), input_shape=imgShape, padding='same', activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(BatchNormalization())
 
 model.add(Conv2D(filters=64, kernel_size=(3,3), input_shape=imgShape, padding='same', activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-# model.add(BatchNormalization())
+
+model.add(Conv2D(filters=64, kernel_size=(3,3), input_shape=imgShape, padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(filters=64, kernel_size=(3,3), input_shape=imgShape, padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Conv2D(filters=64, kernel_size=(3,3), input_shape=imgShape, padding='same', activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -79,15 +96,10 @@ model.add(Conv2D(filters=128, kernel_size=(3,3), input_shape=imgShape, padding='
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
-model.add(Dropout(0.2)) # randomly removes 20% of existing neurons connections to prevent overfitting
 
 model.add(Dense(256))
 model.add(Activation('relu'))
-model.add(Dropout(0.2))
-
-model.add(Dense(128))
-model.add(Activation('relu'))
-model.add(Dropout(0.2))
+model.add(Dropout(0.5)) # randomly removes 50% of existing neurons connections to prevent overfitting
 
 model.add(Dense(len(mappedNames))) # last layer with amount of neurons the same as amount of fruits
 model.add(Activation('softmax')) # Selects the neuron with highest probability
@@ -95,6 +107,46 @@ model.add(Activation('softmax')) # Selects the neuron with highest probability
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 print(model.summary())
+
+### OTHER MODEL
+
+# shape_img = (150, 150, 3)
+#
+# model2 = Sequential()
+#
+# model2.add(Conv2D(filters=32, kernel_size=(3, 3), input_shape=shape_img, activation='relu', padding='same'))
+# model2.add(MaxPooling2D(pool_size=(2, 2)))
+#
+# model2.add(Conv2D(filters=64, kernel_size=(3, 3), input_shape=shape_img, activation='relu', padding='same'))
+# model2.add(MaxPooling2D(pool_size=(2, 2)))
+#
+# model2.add(Conv2D(filters=64, kernel_size=(3, 3), input_shape=shape_img, activation='relu', padding='same'))
+# model2.add(MaxPooling2D(pool_size=(2, 2)))
+#
+# model2.add(Conv2D(filters=64, kernel_size=(3, 3), input_shape=shape_img, activation='relu', padding='same'))
+# model2.add(MaxPooling2D(pool_size=(2, 2)))
+#
+# model2.add(Conv2D(filters=64, kernel_size=(3, 3), input_shape=shape_img, activation='relu', padding='same'))
+# model2.add(MaxPooling2D(pool_size=(2, 2)))
+#
+# model2.add(Conv2D(filters=64, kernel_size=(3, 3), input_shape=shape_img, activation='relu', padding='same'))
+# model2.add(MaxPooling2D(pool_size=(2, 2)))
+#
+# model2.add(Flatten())
+#
+# model2.add(Dense(256))
+# model2.add(Activation('relu'))
+# model2.add(Dropout(0.5))
+#
+# model2.add(Dense(len(mappedNames)))
+# model2.add(Activation('softmax'))
+#
+# model2.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+##########################
+
+
+
 
 # loading the images
 fruitImages = []
@@ -111,11 +163,11 @@ for idx, path in enumerate(imgTrain):
     fruitImages.append(loadedImage)
     fruitLabels.append(loadedLabel)
     loadedImgCtr += 1
-    print("Loaded img {}".format(loadedImgCtr))
+    if loadedImgCtr % 100 == 0:
+        print("Loaded img {}".format(loadedImgCtr))
 
 fruitImages = np.array(fruitImages)
 fruitLabels = np.array(fruitLabels)
-
 
 print("Training the model")
 
@@ -132,14 +184,51 @@ plt.legend(['train', 'test'], loc='upper left')
 # plt.ylim(0, 1)
 plt.show()
 
-del fruitImages
-del fruitLabels
+
+#### model 2
+
+# history = model2.fit(fruitImages, fruitLabels, validation_split=0.1, epochs=100, batch_size=128)
+# # summarize history for accuracy
+# plt.figure()
+# plt.plot(history.history['accuracy'])
+# plt.plot(history.history['val_accuracy'])
+# plt.title('model accuracy')
+# plt.ylabel('accuracy')
+# plt.xlabel('epoch')
+# plt.legend(['train', 'test'], loc='upper left')
+# # plt.ylim(0.5, 1)
+# # plt.ylim(0, 1)
+# plt.show()
+
+##############
 
 # scores = model.evaluate(imgTest, labelTest, verbose=0)
 # print("Accuracy: %.2f%%" % (scores[1]*100))
 
-filename = 'my_model.sav'
+filename = 'my_model3.sav'
 pickle.dump(model, open(filename, 'wb'))
+
+del fruitImages
+del fruitLabels
 
 endTime = time.time()
 print("Processing images and training the model took {} seconds".format(endTime - startTime))
+print("end")
+
+for idx, path in enumerate(imgTest):
+    loadedImage = plt.imread(path)
+    loadedImage = cv2.resize(loadedImage,(150,150))
+
+    loadedLabel = labelTest[idx]
+
+    fruitImages.append(loadedImage)
+    fruitLabels.append(loadedLabel)
+    loadedImgCtr += 1
+    if loadedImgCtr % 100 == 0:
+        print("Loaded img {}".format(loadedImgCtr))
+
+fruitImagesTest = np.array(fruitImages)
+fruitLabelsTest = np.array(fruitLabels)
+
+scores = model.evaluate(fruitImagesTest, fruitLabelsTest, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
